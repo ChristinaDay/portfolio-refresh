f54fd0f8ecdc086ac71fb95fffe9f7adc7494e8f
Christina Day <christinamday@gmail.com>
2015-09-19 03:13:09 -0700

    update gulpfile and remove unnecessary packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,51 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
 var scsslint = require('gulp-scss-lint');
-var browserSync = require('browser-sync').create();
 var jade = require('gulp-jade');
 var jadelint = require('gulp-jadelint');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 var plumber = require('gulp-plumber');
 
-gulp.task('sass', ['scss-lint'], function () {
-  gulp.src('./app/styles/**/*.scss')
+var paths = {
+  styles: './app/styles/**/*.scss',
+  topLevelTemplates: './app/templates/*.jade',
+  templates: './app/templates/**/*.jade',
+  build: './build',
+  buildTemplates: './build/*.html'
+};
+
+gulp.task('sass', function () {
+  gulp.src(paths.styles)
     .pipe(plumber())
+    .pipe(scsslint())
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(prefix())
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest(paths.build))
     .pipe(browserSync.stream());
 });
 
-gulp.task('scss-lint', function() {
-  gulp.src('./app/styles/**/*.scss')
-    .pipe(scsslint());
-});
-
-gulp.task('jade', ['jade-lint'], function() {
-  gulp.src('./app/templates/*.jade')
+gulp.task('jade', function() {
+  gulp.src(paths.topLevelTemplates)
     .pipe(plumber())
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./build'))
-})
-
-gulp.task('jade-lint', function() {
-  gulp.src('./app/templates/**/*.jade')
-    .pipe(jadelint());
-});
-
-gulp.task('browser-sync', function() {
-
+    .pipe(jadelint())
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('serve', function () {
   browserSync.init({
       server: {
-          baseDir: "./build"
+          baseDir: paths.build
       }
   });
 
-  gulp.watch('./app/styles/**/*.scss', ['sass']);
-  gulp.watch('./app/templates/**/*.jade', ['jade'])
-  gulp.watch('./build/*.html').on("change", browserSync.reload);
+  gulp.watch(paths.styles, ['sass']);
+  gulp.watch(paths.templates, ['jade']);
+  gulp.watch(paths.buildTemplates).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
