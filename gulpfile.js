72f9794144827a116131fb07a9a8df797a445f27
Christina Day <christinamday@gmail.com>
2015-09-18 22:22:24 -0700

    add browsersync, setup templates

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,20 @@ var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var scsslint = require('gulp-scss-lint');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
 var jade = require('gulp-jade');
+var jadelint = require('gulp-jadelint');
+var plumber = require('gulp-plumber');
 
-gulp.task('sass', function () {
+gulp.task('sass', ['scss-lint'], function () {
   gulp.src('./app/styles/**/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(prefix())
     .pipe(gulp.dest('./build'))
-    .pipe(livereload());
+    .pipe(browserSync.stream());
 });
 
 gulp.task('scss-lint', function() {
@@ -21,18 +24,36 @@ gulp.task('scss-lint', function() {
     .pipe(scsslint());
 });
 
-gulp.task('jade', function() {
-  gulp.src('./app/templates/**/*.jade')
-    .pipe(jade())
+gulp.task('jade', ['jade-lint'], function() {
+  gulp.src('./app/templates/*.jade')
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
     .pipe(gulp.dest('./build'))
 })
 
-gulp.task('watch', ['scss-lint'], function () {
-  livereload.listen();
-  gulp.watch('./app/styles/**/*.scss', ['sass']);
-  gulp.watch('./app/templates/**/*.jade', ['jade'])
+gulp.task('jade-lint', function() {
+  gulp.src('./app/templates/**/*.jade')
+    .pipe(jadelint());
 });
 
-gulp.task('default', ['watch']);
+gulp.task('browser-sync', function() {
+
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+      server: {
+          baseDir: "./build"
+      }
+  });
+
+  gulp.watch('./app/styles/**/*.scss', ['sass']);
+  gulp.watch('./app/templates/**/*.jade', ['jade'])
+  gulp.watch('./build/*.html').on("change", browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
 
 
